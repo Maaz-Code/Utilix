2f6128298241fb6bd6950f9ef3494290eafff0af
Maaz-Code <500077097@stu.upes.ac.in>
2021-06-27 15:28:21 +0530

    Next Commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,24 @@
-console.log(firebase);
+const auth = firebase.auth();
+
+const whenSignedIn = document.getElementById('whenSignedIn');
+const whenSignedOut = document.getElementById('whenSignedOut');
+
+const signInbtn = document.getElementById('signInbtn');
+const signOutbtn = document.getElementById('signOutbtn');
+
+
+const provider = new firebase.auth.GoogleAuthProvider();
+signInbtn.onclick = () => auth.signInWithPopup(provider);
+signOutbtn.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged (user => {
+    if (user) {
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        alert('Signed in!');
+    } 
+    else {
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+    }
+});
